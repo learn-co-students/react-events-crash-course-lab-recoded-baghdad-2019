ab94936aca568d65d2cae68596aba2442eaccd3e
Mohammed Jawad <al7joker111@gmail.com>
2019-12-10 16:55:37 +0300

    Dony

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -42,7 +42,7 @@ export default class ChromeBoisDomain extends Component {
       <canvas 
         onMouseMove={this.handleMouseMove}
         onKeyDown ={this.handleKey}
-        onClick={this.handleKey}
+        onClick={this.handleClick}
         width='900'
         height='600'
         tabIndex="0">
